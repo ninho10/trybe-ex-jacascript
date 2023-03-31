8b44be7babe8e1f378dd82e67e349dda8665eef5
ninho10 <paulo.trabalho10@gmail.com>
2023-03-31 01:33:13 -0300

    coloque mas uma variavel no segundo commit do trabalho da trybe

---
+++ ex004.js
@@ -0,0 +1,16 @@
+//Qual exemplo abaixo apresenta uma boa estrutura de declaração da variável?
+
+let idade = 34;
+
+// A forma adequada de atribuição de um novo valor à variável abaixo, que já está declarada, é encontrada em:
+
+let escola = 'trybe';
+
+escola = 'que te ensina a programar'
+
+
+let casa = 'madureira';
+
+console.log(typeof (casa));
+
+
