2d2ed2efbf9e1fb221c4ba2a8f957ecccca846f5
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-01 10:44:55 -0300

    Primeiro commit do ex005 sobre operadores aritméticos

---
+++ ex005.js
@@ -0,0 +1,20 @@
+// Operadores Aritméticos
+
+// Operação de soma:
+2 + 2; // 4
+
+// Operação de subtração:
+10 - 2; // 8
+
+// Operação de multiplicação:
+4 * 3; // 12
+
+// Operação de divisão:
+let divisao = 12 / 6; // 2
+
+console.log(divisao)
+
+
+// orde  parrênteses, Exponenciação **, Multiplicação e divisão, Adição e Subtração
+
+
