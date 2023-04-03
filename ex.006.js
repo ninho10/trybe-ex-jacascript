daaa5968d64bdda5d5e3e64ef52cdee11c868a8d
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-03 15:37:54 -0300

    commit do ex 6 de javascript da trybe

---
+++ ex.006.js
@@ -0,0 +1,34 @@
+
+// === para comparar estritamente a igualdade entre dois valores;
+// !== para comparar estritamente a diferença entre dois valores;
+// > para comparar se um valor é maior do que o outro;
+// < para comparar se um valor é menor do que o outro;
+// >= para comparar se um valor é maior ou igual do que o outro;
+// <= para comparar se um valor é menor ou igual do que o outro.
+
+let valor1 = 7 < 10; // 7 é menor que 10 (verdadeiro), então retorna true.
+
+let valor2 = 7 > 10; // 7 é maior que 10 (falso), então retorna false. 
+
+console.log(valor1, valor2);
+
+
+const cidade = 'Belo Horizonte';
+
+
+const verificaCidade = cidade === 'Belo Horizonte';
+
+
+console.log(verificaCidade);
+
+
+
+let roupa1 = 35;
+
+let roupa2 = 20;
+
+let comparar = roupa1 + roupa2;
+
+let orcamento = comparar <= 60;
+
+console.log(orcamento);
