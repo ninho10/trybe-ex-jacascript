e36c19cf886c77319de67551ea3d5b463d2d85db
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-04 18:10:33 -0300

    ex007 da trybe Operadores lógicos

---
+++ ex007.js
@@ -0,0 +1,35 @@
+/*
+let ensolarado = true;
+let fimDeSemana = false;
+let vaiAPraia = ensolarado && fimDeSemana
+console.log(vaiAPraia); // false
+
+
+let gasolina = true;
+let dinheiro = false;
+let vaiAPraia2 = gasolina || dinheiro;
+console.log(vaiAPraia2); // true
+
+
+let carroQuebrado = true;
+let vaiAPraia3 = !carroQuebrado;
+console.log(vaiAPraia3); // false
+
+
+console.log(!true)
+
+*/
+
+//Neste exercício você vai desenvolver um programa que verifica se um 
+//número está no intervalo entre 20 e 50 e se o número é par. Abaixo existe 
+//uma variável com o número a ser verificado:
+
+let numero = 30;
+
+let resultado = numero >= 20 && numero<= 50 && numero % 2 == 0;
+console.log(resultado);
+
+
+
+let local = 'praia'
+let estadoEmocional = true
