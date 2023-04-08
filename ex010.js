6ad83a7c0a0f9bf7934e15afedb976cd144c2d94
Paulo Roberto <paulo.trabalho10@gmail.com>
2023-04-07 22:01:04 -0300

    Create ex010.js
    ex010 terminada da trybe sobre arrays.

---
+++ ex010.js
@@ -0,0 +1,34 @@
+//                            Arrays
+
+let alunosTurma1 = ["Fàbio", "Cristina", "Jéssica"];
+let alunosTurma2 = ["João", "Diogo"];
+
+console.log(alunosTurma1); // ['Fàbio', 'Cristina', 'Jéssica']
+console.log(alunosTurma2); // ['João', 'Diogo']
+
+let listaDeCompras = ["arroz", "feijão", "ovo"];
+listaDeCompras[3] = "farofa";
+console.log(listaDeCompras);
+
+const listaCompras = ["arroz", "feijão", "ovo"];
+console.log(listaCompras.push("farofa")); // ['arroz', 'feijão', 'ovo', 'farofa']
+
+let familia = ["paulo", "Rosilda"];
+familia.push("juliana");
+familia.push("Davi");
+familia.push("Jovan");
+console.log(familia);
+
+const ingredientes = ['arroz', 'batata', 'cenoura', 'sal', 'pimenta do reino'];
+ingredientes[2] = 'suco';
+console.log(ingredientes); // ['arroz', 'batata', 'suco', 'sal', 'pimenta do reino']
+
+
+
+
+let lista = [1, 2, 3, 4, 5];
+console.log(lista.reverse())
+
+
+
+
